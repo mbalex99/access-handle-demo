771601a8a091b32df48310d4498349c59589a296
Maximilian Alexander <max@ditto.live>
2022-01-30 02:07:45 -0700

    adding gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/access-handle-demo/', // just for gh-pages
   plugins: [reactRefresh()]
 })
